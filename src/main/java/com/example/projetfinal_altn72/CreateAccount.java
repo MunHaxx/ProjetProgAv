5be6d4d6ad34928c2aeca8ed61cdc5004d79147a
Gigik <nelgie.ferrol94@gmail.com>
2023-10-12 10:04:05 +0200

    Try adding to my branch

---
+++ src/main/java/com/example/projetfinal_altn72/CreateAccount.java
@@ -0,0 +1,6 @@
+
+
+@WebServlet(name="", value="")
+public class CreateAccount{
+
+}
---
+++ src/main/java/com/example/projetfinal_altn72/Login.java
@@ -0,0 +1,11 @@
+package com.example.projetfinal_altn72;
+
+import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+
+@WebServlet(name= "login", value="/login")
+public class Login{
+
+}

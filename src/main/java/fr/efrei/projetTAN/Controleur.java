9353e5c5218b5e819c252f3f61161d8080431b66
Aunorine <chloe.bastong@efrei.net>
2023-10-21 18:12:48 +0200

    Création 1ière Entity et sessionBean + tests dans le contrôleur
    Suppression de Employe (restes du TP3)

--- src/main/java/fr/efrei/projetTAN/Controleur.java
+++ src/main/java/fr/efrei/projetTAN/Controleur.java
@@ -1,35 +1,39 @@
 package fr.efrei.projetTAN;
 
 import java.io.*;
-import java.util.List;
 
-import fr.efrei.projetTAN.model.EmployesEntity;
-import fr.efrei.projetTAN.model.EmployesSessionBean;
-import fr.efrei.projetTAN.utils.EmployesConstantes;
+import fr.efrei.projetTAN.entities.CompetenceEntity;
+import fr.efrei.projetTAN.session.CompetenceSessionBean;
+import fr.efrei.projetTAN.utils.CompetenceConst;
 import jakarta.ejb.EJB;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
-import static fr.efrei.projetTAN.utils.EmployesConstantes.*;
+import static fr.efrei.projetTAN.utils.CompetenceConst.*;
 
-//@WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class Controleur extends HttpServlet {
-    @EJB
-    private EmployesSessionBean employesSessionBean;
     private Utilisateur unUtilisateur;
-    private List<EmployesEntity> tousLesEmployes;
-    private EmployesEntity employe;
     private String actionUtilisateur;
-    private int idEmployeSelect;
+    @EJB
+    private CompetenceSessionBean competenceSessionBean;
 
-    public void processRequest (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    public void init() {
+        // Laisser cette fonction vide
+        // Je l'ai utilisé pour mes tests avec des System.println
+        /*testerRecupToutesCompetences();
+        testerRecupUneCompetence(1);
+        testerModificationCompetence(1);
+        testerRecupUneCompetence(1);*/
+    }
+
+    public void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         actionUtilisateur = request.getParameter("action");
-        tousLesEmployes = employesSessionBean.getTousLesEmployes();
-        request.getSession().setAttribute("tousLesEmployes", tousLesEmployes);
+//        tousLesEmployes = employesSessionBean.getTousLesEmployes();
+//        request.getSession().setAttribute("tousLesEmployes", tousLesEmployes);
         chargerLaPageSuivante(actionUtilisateur, request, response);
     }
 
     // Une tâche <-> une méthode
-    public boolean verifierInfosConnexion(Utilisateur unUtilisateur){
+    public boolean verifierInfosConnexion(Utilisateur unUtilisateur) {
         String motDePasseValide = getServletContext().getInitParameter("login");
         String loginValide = getServletContext().getInitParameter("motDePasse");
 
@@ -37,32 +41,32 @@ public class Controleur extends HttpServlet {
                 && unUtilisateur.getMotDePasseSaisi().equals(loginValide));
     }
 
-    public void placerUtilisateurDansContexte(HttpServletRequest request){
+    public void placerUtilisateurDansContexte(HttpServletRequest request) {
         unUtilisateur = new Utilisateur();
         unUtilisateur.setLoginSaisi(request.getParameter(FRM_LOGIN));
         unUtilisateur.setMotDePasseSaisi(request.getParameter(FRM_MDP));
         request.getSession().setAttribute("utilisateur", unUtilisateur);
     }
 
- public void aiguillerVersLaProchainePage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-     if (verifierInfosConnexion(unUtilisateur)) {
-         request.getRequestDispatcher(PAGE_TOUS_LES_EMPLOYES).forward(request, response);
-     } else {
-         request.getSession().setAttribute("messageErreur", MESSAGE_ERREUR_CREDENTIALS_KO);
-         request.getRequestDispatcher(PAGE_INDEX).forward(request, response);
-     }
- }
-
-public void chargerLaPageSuivante(String actionUtilisateur, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (actionUtilisateur == null || actionUtilisateur.isEmpty()){
+    public void aiguillerVersLaProchainePage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (verifierInfosConnexion(unUtilisateur)) {
+            //request.getRequestDispatcher(PAGE_TOUS_LES_EMPLOYES).forward(request, response);
+        } else {
+            request.getSession().setAttribute("messageErreur", MESSAGE_ERREUR_CREDENTIALS_KO);
             request.getRequestDispatcher(PAGE_INDEX).forward(request, response);
-        }else{
-            switch (actionUtilisateur){
+        }
+    }
+
+    public void chargerLaPageSuivante(String actionUtilisateur, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (actionUtilisateur == null || actionUtilisateur.isEmpty()) {
+            request.getRequestDispatcher(PAGE_INDEX).forward(request, response);
+        } else {
+            switch (actionUtilisateur) {
                 case ACTION_LOGIN:
                     placerUtilisateurDansContexte(request);
-                    aiguillerVersLaProchainePage(request,response);
+                    aiguillerVersLaProchainePage(request, response);
                     break;
-                case ACTION_DETAILS:
+                /*case ACTION_DETAILS:
                     idEmployeSelect = Integer.parseInt(request.getParameter(FRM_ID_EMPL_SELECT));
                     employe = employesSessionBean.getEmployeParId(idEmployeSelect);
                     request.getSession().setAttribute("employe", employe);
@@ -94,16 +98,49 @@ public void chargerLaPageSuivante(String actionUtilisateur, HttpServletRequest r
                     employesSessionBean.modifierEmploye(employe);
                     request.getSession().setAttribute("tousLesEmployes", employesSessionBean.getTousLesEmployes());
                     request.getRequestDispatcher(PAGE_TOUS_LES_EMPLOYES).forward(request, response);
-                    break;
+                    break;*/
             }
         }
+    }
 
 
-}
 
-    public void init() { }
 
-    public void destroy() { }
+    // Méthode pour tester la récupération des informations d'un compétence -> A SUPPRIMER !!!
+    public void testerRecupUneCompetence(int id) {
+        System.out.println("\n\n--- Début du test de récupération ---");
+        System.out.println("Test récupération d'une compétence par ID :");
+        int competenceIdToFetch = 1; // Remplacez par l'ID de la compétence que vous voulez récupérer
+        CompetenceEntity competence = competenceSessionBean.getCompetenceParId(competenceIdToFetch);
+        System.out.println("Compétence récupérée - ID : " + competence.getIdCompetence());
+        System.out.println("Nom : " + competence.getNom());
+        System.out.println("Niveau : " + competence.getNiveau());
+    }
+    // Méthode pour tester la récupération des informations des compétences -> A SUPPRIMER !!!
+    public void testerRecupToutesCompetences(){
+        System.out.println("\n\n--- Début du test de récupération ---");
+        System.out.println("Liste de toutes les compétences :");
+        for (CompetenceEntity c : competenceSessionBean.getToutesLesCompetences()) {
+            System.out.println("ID : " + c.getIdCompetence());
+            System.out.println("Nom : " + c.getNom());
+            System.out.println("Niveau : " + c.getNiveau());
+        }
+    }
+    // Méthode pour tester la modification d'une compétence -> A SUPPRIMER !!!
+    public void testerModificationCompetence(int id) {
+        System.out.println("\n\n--- Début du test de modification ---");
+        CompetenceEntity competenceAModifier = competenceSessionBean.getCompetenceParId(id);
+        if (competenceAModifier != null) {
+            competenceAModifier.setNom("NouveauNom");
+            competenceAModifier.setNiveau("NouveauNiveau");
+
+            competenceSessionBean.modifierCompetence(competenceAModifier);
+            System.out.println("Compétence modifiée avec succès : " + competenceAModifier.getIdCompetence());
+        } else {
+            System.out.println("La compétence avec l'ID spécifié n'a pas été trouvée.");
+        }
+    }
+
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         processRequest(request, response);
@@ -112,5 +149,5 @@ public void chargerLaPageSuivante(String actionUtilisateur, HttpServletRequest r
     public void doPost (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         processRequest(request, response);
     }
-
 }
+
---
+++ src/main/java/fr/efrei/projetTAN/entities/CompetenceEntity.java
@@ -0,0 +1,74 @@
+package fr.efrei.projetTAN.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+import static fr.efrei.projetTAN.utils.CompetenceConst.*;
+
+@Entity
+@Table(name = "competence", schema = "prj_progav")
+
+@NamedQueries(
+        {
+                @NamedQuery(name = "recupTousCompt", query = SELECT_TOUTES_LES_COMPETENCES)
+                , @NamedQuery(name = "recupUneCompt", query = SELECT_UNE_COMPETENCE)
+                , @NamedQuery(name = "modifUneCompt", query = MODIF_UNE_COMPETENCE)
+        }
+)
+
+public class CompetenceEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "ID_Competence", nullable = false)
+    private int idCompetence;
+
+    @Basic
+    @Column(name = "Nom", nullable = true, length = 50)
+    private String nom;
+
+    @Basic
+    @Column(name = "niveau", nullable = true, length = 50)
+    private String niveau;
+
+    public CompetenceEntity(Integer id, String nom, String niveau){
+        this.idCompetence = id;
+        this.nom = nom;
+        this.niveau = niveau;
+    }
+
+    public CompetenceEntity() {
+    }
+
+    public int getIdCompetence() { return idCompetence; }
+    public void setIdCompetence(int idCompetence) { this.idCompetence = idCompetence; }
+
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+
+    public String getNiveau() { return niveau; }
+    public void setNiveau(String niveau) { this.niveau = niveau; }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        fr.efrei.projetTAN.entities.CompetenceEntity that = (fr.efrei.projetTAN.entities.CompetenceEntity) o;
+
+        if (idCompetence != that.idCompetence) return false;
+        if (!Objects.equals(nom, that.nom)) return false;
+        return Objects.equals(niveau, that.niveau);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idCompetence;
+        result = 31 * result + (nom != null ? nom.hashCode() : 0);
+        result = 31 * result + (niveau != null ? niveau.hashCode() : 0);
+        return result;
+    }
+}
+
+
